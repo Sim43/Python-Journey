98ded15ca652b6a532579e1ec499103146ca015f
Muhammad Asim Khan <129464567+Sim43@users.noreply.github.com>
2024-09-12 21:21:07 +0500

    added

--- 4. Advanced/01. Blog_Site_Using_Flask_and_Jinja/main.py
+++ 4. Advanced/01. Blog_Site_Using_Flask_and_Jinja/main.py
@@ -1,28 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import os
+from dotenv import load_dotenv
+import smtplib
+
+load_dotenv()
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
 
 app = Flask(__name__)
-all_posts = requests.get("https://api.npoint.io/2378d3d8e0262f7e23d3").json()
 
-@app.route("/")
-def home():
-    return render_template("index.html", posts=all_posts)
+def send_email(name, email, phone, message):
+    message = f"Subject: New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=os.environ['EMAIL'], password=os.environ['PASSWORD'])
+        connection.sendmail(from_addr=os.environ['EMAIL'], to_addrs=os.environ['EMAIL'], msg=message)
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
 
-@app.route('/post/<int:index>')
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        send_email(
+            request.form['name'],
+            request.form['email'],
+            request.form['phone'],
+            request.form['message']
+            )
+        return render_template("contact.html", status=True)
+    return render_template("contact.html", status=False)
+
+
+@app.route("/post/<int:index>")
 def show_post(index):
     requested_post = None
-    for blog_post in all_posts:
+    for blog_post in posts:
         if blog_post["id"] == index:
             requested_post = blog_post
-    return render_template("post.html", requested_post=requested_post)
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
